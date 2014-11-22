600f12e29b18bb44080210b672277a635df6a136
Axel Angel <axel+git@vneko.ch>
2014-11-22 16:54:50 +0100

    Implemented dispatcher for messages, entry add/deletion callback

--- Net/LDAP/SyncDiffListener.pm
+++ Net/LDAP/SyncDiffListener.pm
@@ -2,6 +2,8 @@
 # Copyright (c) 2014 Axel Angel <axel-oss@vneko.ch>. All rights reserved.
 # This program is free software; you can redistribute it and/or
 # modify it under the same terms as Perl itself.
+#
+# LDAP Sync spec: http://tools.ietf.org/html/rfc4533
 
 package Net::LDAP::SyncDiffListener;
 
@@ -16,11 +18,12 @@ use Net::LDAP::Constant qw{LDAP_SYNC_REFRESH_AND_PERSIST};
 
 our $VERSION = '0.1';
 
-sub new($$) {
-    my ($class, $statefile) = @_;
+sub new($$$) {
+    my ($class, $statefile, $callbacks) = @_;
 
     my $obj = bless {}, $class;
     $obj->{statefile} = $statefile;
+    $obj->{callbacks} = $callbacks;
     $obj->{cookie} = "";
     $obj->{entries} = {};
 
@@ -33,16 +36,14 @@ sub new($$) {
     return $obj;
 }
 
-sub listen($$$$) {
-    my ($self, $uri, $search, $callbacks) = @_;
+sub listen($$$) {
+    my ($self, $uri, $search) = @_;
     my $ldap = Net::LDAP->new($uri);
 
     my $sigint = sub {
         print "Clean exit\n";
         $self->save();
         $ldap->disconnect();
-        sleep(1);
-        exit 0;
     };
 
     $SIG{INT} = $sigint;
@@ -74,44 +75,111 @@ sub save {
 
 sub notify {
     my ($self, $message, $entry) = @_;
-    my @controls = $message->control;
-
-    print "Received something type(entry)=", ref $entry, "\n";
-    print "  with ", (scalar @controls), " controls\n";
-
 
     if (not defined $entry) {
         warn "Ignoring undefined entry\n";
     }
     elsif ($entry->isa('Net::LDAP::Intermediate::SyncInfo')) {
-        $self->{cookie} = $entry->{asn}{refreshDelete}{cookie};
-        print "SyncInfo\n";
-        $self->save();
+        $self->handle_syncinfo($message, $entry);
     }
     elsif ($entry->isa('Net::LDAP::Entry')) {
-        print "Regular entry\n";
+        $self->handle_entry($message, $entry);
+    }
+    else {
+        $self->handle_other($message, $entry);
     }
 
-    print "  entry={\n", Dump($entry), "\n}\n" if $entry;
+}
 
-    foreach my $control (@controls) {
-        print "*"x80, "\n";
+sub handle_syncinfo($$$) {
+    my ($self, $message, $entry) = @_;
+    my @controls = $message->control;
+    print "SyncInfo\n";
 
-        print "- Control type ", ref $control, "\n";
-        if ($control->isa('Net::LDAP::Control::SyncState')) {
-            print "  Received Sync State Control\n";
-            print "  entry dns: ", $entry->dn(), "\n";
-            print "  state: ", $control->state, "\n";
-            print "  entryUUID: ", $control->entryUUID, "\n";
-        } elsif ($control->isa('Net::LDAP::Control::SyncDone')) {
-            print "  Received Sync Done Control\n";
-            print "  refreshDeletes: ", $control->refreshDeletes, "\n";
+    my $cookie = $entry->{asn}{refreshDelete}{cookie};
+    if (defined $cookie) {
+        $self->{cookie} = $cookie;
+        $self->save();
+    }
+
+    warn "Unexpected SyncInfo with controls, ignored" if @controls;
+}
+
+sub handle_entry($$$) {
+    my ($self, $message, $entry) = @_;
+
+    my @controls = $message->control;
+    my $control_count = scalar @controls;
+    if ($control_count != 1) {
+        warn "Entry with ", $control_count," controls, skipping";
+        return;
+    }
+
+    print "entry={\n", Dump($entry), "\n}\n" if $entry;
+
+    my $control = $controls[0];
+    if ($control->isa('Net::LDAP::Control::SyncState')) {
+        my $state = $control->state;
+        my $dn = $entry->dn();
+
+        printf "Sync State Control: dispatching: %s (state=%i)\n", $dn, $state;
+
+        if ($state == 0) { # present
+            warn "Entry control says present, nothing to do";
+        }
+        elsif ($state == 1) { # add
+            if (defined $self->{entries}{$dn}) {
+                warn "Entry control says it's new when it's not";
+                $self->handle_entry_changed($entry);
+            }
+            else {
+                $self->{callbacks}{add_entry}($entry);
+                my %attrs = $self->hash_entry($entry);
+                $self->{entries}{$dn} = \%attrs;
+            }
+        }
+        elsif ($state == 2) { # modify
+            $self->handle_entry_changed($entry);
+        }
+        elsif ($state == 3) { # delete
+            $self->{callbacks}{del_entry}($entry);
+            delete $self->{entries}{$dn};
         }
         else {
-            warn "Received something else: ", ref $control, "\n";
+            warn "Entry control unexpected state, ", $state, " ignoring";
         }
+    } elsif ($control->isa('Net::LDAP::Control::SyncDone')) {
+        print "  Received Sync Done Control\n";
+        print "  refreshDeletes: ", $control->refreshDeletes, "\n";
     }
-    print "="x80, "\n";
+    else {
+        warn "Entry with unexpected control type: ", ref $control;
+    }
+}
+
+sub handle_entry_changed($$) {
+    my ($self, $entry) = @_;
+    my $dn = $entry->dn;
+    print "Entry changed ", $dn, "\n";
+
+    # TODO: handle diff here
+
+    my %attrs = $self->hash_entry($entry);
+    $self->{entries}{$dn} = \%attrs;
+}
+
+sub handle_other($$$) {
+    my ($self, $message, $entry) = @_;
+    warn "Unexpected entry type: ", ref $entry;
+}
+
+sub hash_entry($$) {
+    my ($self, $entry) = @_;
+    my %attrs = ();
+    foreach my $key ($entry->attributes) {
+        $attrs{$key} = $entry->get_value($key);
+    }
+    return %attrs;
 }
 
 1;
